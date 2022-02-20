212795251db39d1c45d17521aee9b5aa414c9768
Anna <kireeva625@gmail.com>
2022-02-20 23:00:33 +0400

    F-01 слайдер

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="wrapper">
-    <div class="slider">
-      <div class="overlay"></div>
+    <div class="overlay"></div>
+    <div class="slider" ref="img">
       <div
         v-for="item in slider"
         :key="item.id"
@@ -13,64 +13,118 @@
           <p class="content__descreption">
             {{ item.description }}
           </p>
-          <button class="content__btn">Подробнее</button>
+          <button class="content__btn" :style="{ background: item.background }">
+            Подробнее
+          </button>
         </div>
+        <div class="dote">
+          <div
+            class="dote__item"
+            :class="{ 'dote__item--active': item.id === 0 }"
+          ></div>
+          <div
+            class="dote__item"
+            :class="{ 'dote__item--active': item.id === 1 }"
+          ></div>
+          <div
+            class="dote__item"
+            :class="{ 'dote__item--active': item.id === 2 }"
+          ></div>
+          <div
+            class="dote__item"
+            :class="{ 'dote__item--active': item.id === 3 }"
+          ></div>
+        </div>
+        <button class="action-right" @click="fGetNextImage">
+          <img
+            src="../assets/arrow.svg"
+            alt="Следующий слайд"
+            class="svg-left"
+          />
+        </button>
+        <button class="action-left" @click="fGetPrevImage">
+          <img
+            src="../assets/arrow.svg"
+            alt="Предыдущий слайд"
+            class="svg-right"
+          />
+        </button>
       </div>
-      <div class="dote">
-        <div class="dote__item"></div>
-        <div class="dote__item"></div>
-        <div class="dote__item"></div>
-        <div class="dote__item"></div>
-      </div>
-      <button class="action-right">
-        <img
-          src="../assets/arrow.svg"
-          alt="Предыдущий слайд"
-          class="svg-left"
-        />
-      </button>
-      <button class="action-left">
-        <img
-          src="../assets/arrow.svg"
-          alt="Следующий слайд"
-          class="svg-right"
-        />
-      </button>
     </div>
   </div>
 </template>
 <script lang="ts">
 import { Vue, Component } from "vue-property-decorator";
-
-@Component
+@Component({})
 export default class Slider extends Vue {
-  slider: { id: number; img: string; title: string; description: string }[] = [
+  /* eslint-disable */ //Для объявления типов
+  position: number = 0; /**Положение ленты прокрутки*/
+  frame: number = 1; /**Общее колл-во видимых изображений*/
+  imageWidth: number = 0; /**Ширина видимого окна*/
+  idBtn: number = 0;
+
+  slider: {
+    id: number;
+    img: string;
+    title: string;
+    description: string;
+    background?: string;
+  }[] = [
+    /**Слайдер и его контент */
     {
       id: 0,
       img: require("@/assets/slider-1.png"),
       title: "Бесплатная парковка",
       description:
         "Оставляйте машину на платных городских парковках и разрешенных местах, не нарушая ПДД, а также в аэропортах.",
+      background: "linear-gradient(90deg, #13493F 0%, #0C7B1B 100%)",
     },
+
     {
       id: 1,
       img: require("@/assets/slider-2.png"),
       title: "Страховка",
       description: "Полная страховка страховка автомобиля",
+      background: "linear-gradient(90deg, #132949 0%, #0C7B67 100%)",
     },
     {
       id: 2,
       img: require("@/assets/slider-3.png"),
       title: "Бензин",
       description: "Полный бак на любой заправке города за наш счёт",
+      background: "linear-gradient(90deg, #493013 0%, #7B0C3B 100%)",
     },
     {
       id: 3,
       img: require("@/assets/slider-4.png"),
       title: "Обслуживание",
       description: "Автомобиль проходит еженедельное ТО",
+      background: "linear-gradient(90deg, #281349 0%, #720C7B 100%)",
     },
   ];
+  /**Перейти к предыдущему элементу */
+  fGetPrevImage() {
+    let sliderList = document.querySelector(".slider") as HTMLElement;
+    this.position += this.iImageWidth * this.frame;
+    this.position = Math.min(this.position, 0);
+
+    sliderList.style.marginLeft = this.position + "px";
+  }
+  /**Перейти к следующему слайду */
+  fGetNextImage() {
+    let sliderList = document.querySelector(".slider") as HTMLElement;
+    this.position -= this.imageWidth * this.frame;
+    this.position = Math.max(
+      this.position,
+      -this.iImageWidth * (this.slider.length - this.frame)
+    );
+    sliderList.style.marginLeft = this.position + "px";
+  }
+  /**Получить ширину картинки в зависимости от масштаба для переключения слайда*/
+  get iImageWidth() {
+    let a = (this.$refs.img as HTMLElement).clientWidth;
+    return (this.imageWidth = a);
+  }
 }
 </script>
 
@@ -83,13 +137,15 @@ export default class Slider extends Vue {
 }
 .slider {
   @include content-very-large-main;
+  @include content-large-main;
+  @include content-desctop-standart;
   @include content-desctop--mini;
-  @include slider-table;
   display: flex;
   position: relative;
   height: 100%;
   width: 100%;
   &__img {
+    position: relative;
     min-width: 100%;
     height: 100%;
     background-size: cover;
@@ -121,7 +177,7 @@ export default class Slider extends Vue {
   }
   &__btn {
     @include base-btn;
-    background: $btn-green-dark;
+    // background: $btn-green-dark;
     color: $main-light-gray;
     border-radius: 0.25rem;
     width: 10.1875rem;
@@ -133,9 +189,9 @@ export default class Slider extends Vue {
 }
 .dote {
   position: absolute;
-  top: 95%;
   left: 0;
   right: 0;
+  bottom: -2%;
   margin: auto;
   z-index: 110;
   width: 3.5rem;
@@ -151,6 +207,9 @@ export default class Slider extends Vue {
     height: 0.5rem;
     border-radius: 50%;
   }
+  &__item--active {
+    background-color: $color-green;
+  }
 }
 .action-left {
   @include slider-arrow-hover;
@@ -160,10 +219,7 @@ export default class Slider extends Vue {
 }
 .action-right {
   @include slider-arrow-hover;
-  left: 96.5%;
-  @media (min-width: 1020px) and (max-width: 1280px) {
-    left: 97.5%;
-  }
+  right: 0%;
 }
 .action-right:hover {
   background-color: rgba(14, 194, 97, 0.2);
